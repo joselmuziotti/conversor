6a6b59c1587ee589e84a95cf49b497f4c6e95ba4
joselmuziotti <joselmuziotti@gmail.com>
2024-08-09 11:21:28 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+let userValue = document.getElementById('monto');
+let clpPerUsd = 924.58;
+
+
+
+
+
+
+
+
+
+
+
+// let valorCambio = ['954.89', '856.41', '16.56', '3864.90', '5.02', '0.93', '36.21'];
+// let divisa = ['CLP', 'ARS', 'MXN', 'COP', 'BRL', 'EUR', 'VES'];
+
+//     function btnConvertir() {
+//         const montoCambio = document.getElementById('monto').value;
+//         const tipoDivisa = document.getElementById('opciones').value;
+//         const resultadoInput = document.getElementById('resultados');
+
+//     for (let i = 0; i < divisa.length; i++) {
+//         if (tipoDivisa === divisa[i]) {
+//             const total = montoCambio * valorCambio[i];
+//             resultadoInput.value = total.toFixed(2);
+//         }
+//     }
+//     }
